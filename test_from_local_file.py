8d588ac849168d3c6a378c34bd56904a9941eb4a
Stephan Sturges <stephansturges@Stephans-MacBook-Air.local>
2023-01-03 15:41:30 +0100

    adding auto-crop and loading imgs from /testpics/

--- test_from_local_file.py
+++ test_from_local_file.py
@@ -10,11 +10,13 @@ import cv2
 import depthai as dai
 import numpy as np
 from time import monotonic
+import os
 
 nnPath = "models/20221214_bigkahunaburger_6shavesonly.blob"
 
-mediaPath = "pics/test_google_earth.png"
-# mediaPath = "pics/depth_video.mov"
+#mediaPath = "pics/test_google_earth.png"
+
+mediaPath = "testpics/"
 
 num_of_classes = 3 # define the number of classes in the dataset
 nn_shape = 256
@@ -66,50 +68,72 @@ nn.out.link(nnOut.input)
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
-    # Input queue will be used to send video frames to the device.
-    qIn = device.getInputQueue(name="inFrame")
-    # Output queue will be used to get nn data from the video frames.
-    qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+    for file in [f for f in os.listdir(mediaPath) if os.path.isfile(os.path.join(mediaPath,f)) and f.endswith(".jpg") or f.endswith(".png")]:
+        # Input queue will be used to send video frames to the device.
+        qIn = device.getInputQueue(name="inFrame")
+        # Output queue will be used to get nn data from the video frames.
+        qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
 
-    frame = None
-    output_colors = None
+        frame = None
+        output_colors = None
 
-    def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
-        return cv2.resize(arr, shape).transpose(2, 0, 1).flatten()
+        def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
+            return cv2.resize(arr, shape).transpose(2, 0, 1).flatten()
 
-    cap = cv2.VideoCapture(mediaPath)
-    using_video = cap.isOpened()
-    if using_video:
-        print(f"Using video...{mediaPath}")
-        def test():
-            return cap.isOpened()
-    else:
-        print(f"Using image...{mediaPath}")
-        frame = cv2.imread(mediaPath, cv2.IMREAD_COLOR)
-        def test():
-            return True
-    
-    cv2.namedWindow("rgb", cv2.WINDOW_NORMAL)        
-    while test():
+        cap = cv2.VideoCapture(mediaPath)
+        using_video = cap.isOpened()
         if using_video:
-            read_correctly, new_frame = cap.read()
-            if read_correctly:
-                frame = new_frame
-        img = dai.ImgFrame()
-        img.setData(to_planar(frame, (nn_shape, nn_shape)))
-        img.setTimestamp(monotonic())
-        img.setWidth(nn_shape)
-        img.setHeight(nn_shape)
-        qIn.send(img)
+            print(f"Using video...{os.path.join(mediaPath,file)}")
+            def test():
+                return cap.isOpened()
+        else:
+            print(f"Using image...{os.path.join(mediaPath,file)}")
+            frame = cv2.imread(os.path.join(mediaPath,file), cv2.IMREAD_COLOR)
+                        
+            # Get the dimensions of the image
+            height, width, _ = frame.shape
 
-        inDet = qDet.tryGet()
+            # Check if the image is a square
+            if height == width:
+                # The image is already a square, so we don't need to do anything
+                pass
+            else:
+                # The image is not a square, so we need to crop it
 
-        if inDet is not None:
-            lay1 = np.array(inDet.getFirstLayerInt32()).reshape(nn_shape,nn_shape)
-            output_colors = decode_deeplabv3p(lay1)
+                # Determine which dimension is larger (height or width)
+                larger_dim = max(height, width)
 
-        if (frame is not None) and (output_colors is not None):
-            cv2.imshow("rgb", show_deeplabv3p(output_colors, cv2.resize(frame, (nn_shape,nn_shape))))
+                # Crop the image to a square with the larger dimension
+                if height > width:
+                    # Crop the image along the height
+                    frame = frame[:width, :]
+                else:
+                    # Crop the image along the width
+                    frame = frame[:, :height]
+            def test():
+                return True
+        
+        cv2.namedWindow("rgb", cv2.WINDOW_NORMAL)        
+        while test():
+            if using_video:
+                read_correctly, new_frame = cap.read()
+                if read_correctly:
+                    frame = new_frame
+            img = dai.ImgFrame()
+            img.setData(to_planar(frame, (nn_shape, nn_shape)))
+            img.setTimestamp(monotonic())
+            img.setWidth(nn_shape)
+            img.setHeight(nn_shape)
+            qIn.send(img)
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+            inDet = qDet.tryGet()
+
+            if inDet is not None:
+                lay1 = np.array(inDet.getFirstLayerInt32()).reshape(nn_shape,nn_shape)
+                output_colors = decode_deeplabv3p(lay1)
+
+            if (frame is not None) and (output_colors is not None):
+                cv2.imshow("rgb", show_deeplabv3p(output_colors, cv2.resize(frame, (nn_shape,nn_shape))))
+
+            if cv2.waitKey(1) == ord('q'):
+                break
